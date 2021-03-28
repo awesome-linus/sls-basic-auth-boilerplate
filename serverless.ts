2309b0e7236ec4258049b60400675cd1214a6145
kou <contact@awesome-linus.com>
2021-03-29 01:11:11 +0900

    add basic auth

--- serverless.ts
+++ serverless.ts
@@ -1,7 +1,5 @@
 import type { AWS } from '@serverless/typescript';
 
-import { hello } from './src/functions';
-
 const serverlessConfiguration: AWS = {
   service: 'sls-basic-auth-boilerplate',
   frameworkVersion: '2',
@@ -9,22 +7,52 @@ const serverlessConfiguration: AWS = {
     webpack: {
       webpackConfig: './webpack.config.js',
       includeModules: true
-    }
+    },
   },
   plugins: ['serverless-webpack'],
   provider: {
     name: 'aws',
+    // 2021/03時点では、LambdaEdgeとしてCloudFrontへ紐づける場合は、
+    // node12系、us-east-1でないといけないので注意。
     runtime: 'nodejs12.x',
+    region: 'us-east-1',
+    profile: process.env.AWS_PROFILE,
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
-    environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-    },
     lambdaHashingVersion: '20201221',
+    timeout: 900,
+    role: 'roleLambdaEdge'
   },
-  functions: { hello }
+  functions: { 
+    'basic-auth': {
+      handler: 'src/functions/index.handler',
+      memorySize: 128,
+      timeout: 5,
+      name: 'basic-auth'
+    }
+  },
+  resources: {
+    Resources: {
+      roleLambdaEdge: {
+        Type: 'AWS::IAM::Role',
+        Properties: {
+          AssumeRolePolicyDocument: {
+            Statement: [
+              {
+                Effect: 'Allow',
+                Principal: {
+                  Service: ['lambda.amazonaws.com', 'edgelambda.amazonaws.com']
+                },
+                Action: ['sts:AssumeRole']
+              }
+            ]
+          }
+        }
+      }
+    }
+  }
 }
 
 module.exports = serverlessConfiguration;
